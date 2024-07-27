fe861b46aa66c0f577a1c2af10aa12aa08faf6e2
kamigearx12 <sonics0977703996@gmail.com>
2024-07-27 11:53:45 +0300

    Added Normal(almost) Title
    swith dark mode needed to change on ElevatedButton, for example

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# from msilib.schema import TextStyle
-from ctypes import alignment, sizeof
-from math import exp
-from turtle import color
-from typing import Text
-# from msilib.schema import ListView
 import flet as ft
 
 def main(page: ft.Page):
@@ -13,47 +7,85 @@ def main(page: ft.Page):
         "RobotoBold" : "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf"
     }
 
+    def theme_changed(e):
+        page.theme_mode = (
+            ft.ThemeMode.DARK
+            if page.theme_mode == ft.ThemeMode.LIGHT
+            else ft.ThemeMode.LIGHT
+        )
+        titlebar[0].content = ft.Row([ft.Switch(thumb_icon=ft.icons.DARK_MODE_OUTLINED if page.theme_mode == ft.ThemeMode.LIGHT else ft.icons.DARK_MODE,on_change=theme_changed, active_color='black', active_track_color='white' if page.theme_mode == ft.ThemeMode.DARK else 'black', thumb_color='white' if page.theme_mode == ft.ThemeMode.LIGHT else "black", value=True if page.theme_mode == ft.ThemeMode.DARK else False )], spacing=1, scale=1.5, alignment=ft.MainAxisAlignment.CENTER)
+        titlebar[0].border.top.color = 'white' if page.theme_mode == ft.ThemeMode.DARK else 'black'#type: ignore
+        titlebar[0].bgcolor = 'black' if page.theme_mode == ft.ThemeMode.DARK else 'white'
+        titlebar[0].shadow.color = "white" if page.theme_mode == ft.ThemeMode.DARK else 'black' #type: ignore
+
+        titlebar[1].bgcolor = "black" if page.theme_mode == ft.ThemeMode.DARK else 'white'
+        titlebar[1].shadow.color = "white" if page.theme_mode == ft.ThemeMode.DARK else 'black' #type: ignore
+        titlebar[1].border.top.color = 'white' if page.theme_mode == ft.ThemeMode.DARK else 'black'#type: ignore
+
+        titlebar[2].bgcolor = "black" if page.theme_mode == ft.ThemeMode.DARK else 'white'
+        titlebar[2].shadow.color = "white" if page.theme_mode == ft.ThemeMode.DARK else 'black' #type: ignore
+        titlebar[2].border.top.color = 'white' if page.theme_mode == ft.ThemeMode.DARK else 'black'#type: ignore
+        titlebar[2].content = ft.Row([ft.IconButton(ft.icons.TELEGRAM, 'black' if page.theme_mode == ft.ThemeMode.LIGHT else 'white',expand=False, icon_size=50),ft.IconButton(ft.icons.KEYBOARD_OPTION_KEY_OUTLINED, 'black' if page.theme_mode == ft.ThemeMode.LIGHT else 'white',expand=False, icon_size=50),ft.IconButton(ft.icons.TIKTOK, 'black' if page.theme_mode == ft.ThemeMode.LIGHT else 'white',expand=False, icon_size=50)], spacing=1, alignment=ft.MainAxisAlignment.CENTER)
+        page.update()
+
+    titlebar = [
+        ft.Container(
+            content=ft.Row([ft.Switch(thumb_icon=ft.icons.DARK_MODE,on_change=theme_changed, active_color='white', active_track_color='white', thumb_color='black')], spacing=1, scale=1.5, alignment=ft.MainAxisAlignment.CENTER),
+            border=ft.border.all(3, 'black'),
+            padding=13, #type: ignore
+            expand=False,
+            width=100, #type: ignore
+            border_radius=50,
+            bgcolor='white',
+            shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,2))
+        ),
+
+        ft.Container(
+            content=ft.Text(
+                "Welcome to MusDev!", 
+                size=51,
+                weight=ft.FontWeight.W_500, 
+                font_family="RobotoBold",
+                selectable=True
+            ),
+            alignment=ft.alignment.center,
+            padding=25, #type: ignore
+            border=ft.border.all(3, color='black'),
+            border_radius=100,
+            expand=False,
+            width=600, #type: ignore
+            bgcolor='white',
+            shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,2))
+        ),
+
+        ft.Container(
+            content=ft.Row([ft.IconButton(ft.icons.TELEGRAM, 'black',expand=False, icon_size=50),ft.IconButton(ft.icons.KEYBOARD_OPTION_KEY_OUTLINED, 'black',expand=False, icon_size=50),ft.IconButton(ft.icons.TIKTOK, 'black',expand=False, icon_size=50)], spacing=1, alignment=ft.MainAxisAlignment.CENTER),
+            border=ft.border.all(3, 'black'),
+            padding=page.height / 250, #type: ignore
+            expand=False,
+            width=220, #type: ignore
+            border_radius=50,
+            bgcolor='white',
+            shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,2))
+        )
+    ]
+
     Block1: ft.Container = ft.Container(
         content=
             ft.Row(
                 controls=[
                     ft.Container(
                         content=ft.Row([
-                            ft.Container(
-                                content=ft.Text(
-                                    "Welcome to MusDev!", 
-                                    size=75,
-                                    weight=ft.FontWeight.W_500, 
-                                    font_family="RobotoBold",
-                                    selectable=True
-                                ),
-                                alignment=ft.alignment.center,
-                                padding=page.height / 50, #type: ignore
-                                border=ft.border.all(5, color='black'),
-                                border_radius=100,
-                                expand=False,
-                                width=page.width / 2.5, #type: ignore
-                                bgcolor='white',
-                                shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,5))
-                            ),
-                            ft.Container(
-                                content=ft.Row([ft.IconButton(ft.icons.TELEGRAM, 'black',expand=False, icon_size=50),ft.IconButton(ft.icons.KEYBOARD_OPTION_KEY_OUTLINED, 'black',expand=False, icon_size=50),ft.IconButton(ft.icons.TIKTOK, 'black',expand=False, icon_size=50)], spacing=1),
-                                border=ft.border.all(5, 'black'),
-                                padding=page.height / 250, #type: ignore
-                                expand=False,
-                                width=page.width / 10, #type: ignore
-                                border_radius=50,
-                                bgcolor='white',
-                                shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,5))
-                            )
-                        ], alignment=ft.MainAxisAlignment.CENTER),
-                        border=ft.border.all(5, 'black'),
+                            titlebar[0],titlebar[1],titlebar[2]
+                        ], alignment=ft.MainAxisAlignment.CENTER, spacing=25),
+                        # border=ft.border.all(5, 'black'),
                         opacity=0.7,
                         border_radius=15,
-                        bgcolor='white',
-                        shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,5)),
-                        padding=page.height / 75, #type: ignore
-                        width=page.width / 1.79, #type: ignore
+                        # bgcolor='white',
+                        # shadow=ft.BoxShadow(color='black', blur_radius=5,spread_radius=0.5,offset=(0,5)),
+                        padding=2, #type: ignore
+                        # width=page.width, #type: ignore
+                        expand=True
                     )
                 ], alignment=ft.MainAxisAlignment.CENTER
             ),
@@ -64,7 +96,8 @@ def main(page: ft.Page):
 
     pagelist: ft.ListView = ft.ListView(
         controls=[
-            Block1,ft.Divider(color='black', height=2, thickness=3, opacity=0.5, leading_indent=page.width / 5, trailing_indent=page.width / 5)#Block1    #type: ignore
+            Block1,
+            ft.Divider(color='black', height=2, thickness=3, opacity=0.5, leading_indent=450, trailing_indent=450)#Block1    #type: ignore
         ],
 
 
